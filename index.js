c8755b3357e7f0fc2943b8b9a6f0efdbe53b68e3
Fabian Pena <camilo.fabis@gmail.com>
2020-11-10 21:47:42 -0500

    katas

--- index.js
+++ index.js
@@ -45,3 +45,12 @@ function reverseWords(str){
   }
   return reverse.join(" ")
 }
+
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/javascript
+
+function removeUrlAnchor(url){
+  return url.split('#')[0]
+}
